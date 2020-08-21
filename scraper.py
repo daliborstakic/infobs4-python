5cc35dd6766ca8ef9a7792edce0fdf6fb78ece68
daliborstakic <stakicdalibor64@gmail.com>
2020-08-21 13:05:49 +0200

    program writes entries into .csv file

--- scraper.py
+++ scraper.py
@@ -1,5 +1,7 @@
 from bs4 import BeautifulSoup
+from collections import Counter
 import requests
+import csv
 
 # Page source 
 source = requests.get('http://www.informer.rs/vesti').text
@@ -10,20 +12,20 @@ soup = BeautifulSoup(source, 'lxml')
 # Getting the text from h2 and h5 tags
 header_text = soup.find_all(['h5'])
 
+# Empty data
+data = []
+
+# Appending every text
 for header in header_text:
     try:
-        print(header.a.string)
+        text = header.a.string.lower().split(' ')
+        data += text
     except:
         pass
 
-# print(header_text)
-
-# print(link_text)
-
-""" for header in header_text.get_all('a'):
-    try:
-        print(header.a['title'])
-    except:
-        pass """
-
-# print(header_text)
+# CSV File
+with open('infodata.csv', 'w', encoding='utf-8', newline='\n') as csv_file:
+    writer = csv.writer(csv_file)
+    
+    for line in data:
+        writer.writerow(line)
