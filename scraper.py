0b091e5f06392cb20c09eeb52a010177b9c4cd03
daliborstakic <stakicdalibor64@gmail.com>
2020-08-21 12:12:26 +0200

    succesfully scraped headline text

--- scraper.py
+++ scraper.py
@@ -2,7 +2,28 @@ from bs4 import BeautifulSoup
 import requests
 
 # Page source 
-source = requests.get('http://www.informer.rs')
+source = requests.get('http://www.informer.rs/vesti').text
 
 # Soup parser
 soup = BeautifulSoup(source, 'lxml')
+
+# Getting the text from h2 and h5 tags
+header_text = soup.find_all(['h5'])
+
+for header in header_text:
+    try:
+        print(header.a.string)
+    except:
+        pass
+
+# print(header_text)
+
+# print(link_text)
+
+""" for header in header_text.get_all('a'):
+    try:
+        print(header.a['title'])
+    except:
+        pass """
+
+# print(header_text)
